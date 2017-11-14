23b315f476989ee34183959e219e225758fcb050
andrew_omelchenko <omelchenko.sp@gmail.com>
2017-11-14 15:44:33 +0200

    final

--- js/directives/appInfo.js
+++ js/directives/appInfo.js
@@ -4,6 +4,21 @@ app.directive('appInfo', function() {
     scope: { 
       info: '=' 
     }, 
-    templateUrl: 'js/directives/appInfo.html' 
+    templateUrl: 'js/directives/appInfo.html',
+    link: function(scope, element, attrs) { 
+      scope.buttonText = "Install", 
+      scope.installed = false, 
+
+      scope.download = function() { 
+        element.toggleClass('btn-active'); 
+        if (scope.installed) { 
+          scope.buttonText = "Install"; 
+          scope.installed = false; 
+        } else { 
+          scope.buttonText = "Uninstall"; 
+          scope.installed = true; 
+        } 
+      } 
+    } 
   }; 
 });
